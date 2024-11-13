3504d1203f33344008694487be71bafd1595893f
Hyo je Sung <77618270+Heukma@users.noreply.github.com>
2024-11-13 18:03:36 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 퍼즐 게임 챌린지, Time: 28.78 ms, Memory: 25.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지.cpp
@@ -0,0 +1,48 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+int b_min = 1;
+int b_max = 100000;
+long long howmuchtime(int level, vector<int> diffs, vector<int> times){
+    long long time = 0;
+    long long time_prev =0;
+    for(int i=0;i<diffs.size();i++){
+        if(i>=2){
+            time_prev -= times[i-2];
+        }
+        if(diffs[i]<=level){
+            time += times[i];
+        }
+        
+        else{
+            time += (time_prev + times[i])*(diffs[i] - level) + times[i];       
+        }
+        
+        time_prev += times[i];
+    }
+    return time;
+}
+void binary_search(long long time, long long limit, int level){
+        if(time>=limit){
+            b_min = level;
+        }
+        else{
+            b_max = level;
+        }
+}
+int solution(vector<int> diffs, vector<int> times, long long limit) {
+    int answer = 0;
+    int t=1000;
+    while(1){
+        long long level = (b_min + b_max)/2;
+        //cout << b_min << ' ' << b_max << '\n';
+        if(b_min+1 == b_max) break;
+        long long temp = howmuchtime(level,diffs,times);
+        binary_search(temp,limit,level);
+    }
+    if(howmuchtime(b_min,diffs,times) <= limit)
+        return b_min;
+    return b_max;
+}
