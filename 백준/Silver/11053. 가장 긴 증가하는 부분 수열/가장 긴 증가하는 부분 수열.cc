c21e3b37ebf711694c824cf00e6d3ab61dc6e9ed
Hyo je Sung <77618270+Heukma@users.noreply.github.com>
2024-11-14 17:31:45 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.cc
@@ -0,0 +1,29 @@
+#include <string>
+#include <vector>
+#include <deque>
+#include <iostream>
+#include <cstring>
+using namespace std;
+int N;
+int dp[1001] = {};
+int main() {
+	cin >> N;
+	fill(dp, dp+1001, 1);
+	int ans = 0;
+
+	vector <int> sequence(N,0);
+	for (int i = 0; i < N; i++) {
+		cin >> sequence[i];
+	}
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < i; j++) {
+			if (sequence[j] < sequence[i]) {
+				dp[i] = max(dp[i], dp[j] + 1);
+			}
+		}
+		ans = max(ans, dp[i]);
+		/*cout << i << ": " << dp[i] << '\n';*/
+	}
+
+	cout << ans;
+}
