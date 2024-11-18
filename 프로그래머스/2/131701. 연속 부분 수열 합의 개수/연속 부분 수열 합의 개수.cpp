3900152342d00434be260e2f66ab5cb6892ed6fe
Hyo je Sung <77618270+Heukma@users.noreply.github.com>
2024-11-18 16:23:54 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 157.16 ms, Memory: 18.9 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.cpp
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.cpp
@@ -1,39 +1,22 @@
 #include <string>
 #include <vector>
+#include <unordered_set>
 #include <iostream>
 using namespace std;
-int mm[1000000] = {};
-int answer = 0;
+unordered_set<int> uniqueSums;
 vector<int> global_e;
-vector <int> solve(vector <int> a, int new_){
-    vector<int> ret;
-    ret.push_back(new_);
-    if(mm[new_] == 0){
-        answer++;
-        //cout << new_ << '\n';
-        mm[new_] = 1;
+vector<int> solve(vector <int> a, int new_, bool greater){
+	vector<int> ret;
+    if(greater){
+        ret.push_back(new_);
+        uniqueSums.insert(new_);
+    }
+    else{
+        a.pop_back();
     }
     for(int i=0;i<a.size();i++){
         ret.push_back(a[i]+new_);
-        if(mm[a[i]+new_] == 0){
-            answer++;
-            mm[a[i]+new_] = 1;
-            //cout << a[i]+new_ << '\n';
-        }
-    }
-    return ret;
-}
-
-vector <int> solve2(vector <int> a, int new_){
-    vector<int> ret;
-    a.pop_back();
-    for(int i=0;i<a.size();i++){
-        ret.push_back(a[i]+new_);
-        if(mm[a[i]+new_] == 0){
-            answer++;
-            mm[a[i]+new_] = 1;
-            //cout << a[i]+new_ << '\n';
-        }
+        uniqueSums.insert(a[i]+new_);
     }
     return ret;
 }
@@ -43,12 +26,12 @@ int solution(vector<int> elements) {
     global_e = elements;
     vector<int> solvev;
     for(int i=0;i<elements.size();i++){
-        solvev = solve(solvev, elements[i]);
+        solvev = solve(solvev, elements[i], true);
     }
     solvev.pop_back();
-    //solvev.pop_back();
     for(int i=0;i<elements.size() - 2;i++){
-        solvev = solve2(solvev, elements[i]);
+        solvev = solve(solvev, elements[i], false);
     }
-    return answer;
+    return uniqueSums.size();
 }
+
