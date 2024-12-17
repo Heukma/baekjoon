87181a3856eb2824f2eb1fee2cd4d834fb50dc81
Hyo je Sung <77618270+Heukma@users.noreply.github.com>
2024-12-17 14:14:30 +0900

    [Gold V] Title: 탑, Time: 112 ms, Memory: 6308 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.cc
+++ 백준/Gold/2493. 탑/탑.cc
@@ -12,15 +12,12 @@ deque <int> d;
 deque <pair<int,int>> ans;
 int main() {
 	cin >> N;
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	int count = 0;
 	for (int i = 0; i < N; i++) {
 		int temp;
 		cin >> temp;
-		d.push_back(temp);
-	}
-	int count = 0;
-	while (d.size() != 0) {
-		int temp = d[0];
-		d.pop_front();
 		bool flag = false;
 		while(ans.size()!=0) {
 			if (ans[0].first > temp) {
